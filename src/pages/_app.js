26e6e78bb491532efcbcc829cd8172ba3d4bd6d1
Kevin Segovia <ksegovia555@gmail.com>
2023-04-29 12:00:07 -0600

    refactor: _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,9 +9,9 @@ export default function App ({ Component, pageProps }) {
   return (
 
     <>
-    <CookiesProvider>
-      <Component {...pageProps} />
-      <ToastContainer />
+      <CookiesProvider>
+        <Component {...pageProps} />
+        <ToastContainer />
       </CookiesProvider>
     </>
 
