3eb58fe399a9405b3d23334d10579d0d5b0eb032
Kevin Segovia <ksegovia555@gmail.com>
2023-05-19 19:09:22 -0600

    fix: enviroment variable

--- src/services/config.js
+++ src/services/config.js
@@ -3,5 +3,5 @@ export const DB_NAME = process.env.DB_NAME || 'ferreteria'
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'root'
 export const DB_PORT = process.env.DB_PORT || 3306
-export const CLIENTID = process.env.ID_CLIENTE || ''
-export const CLIENTSECRET = process.env.SECRET_CLIENTE || ''
+export const CLIENTID = process.env.NEXT_PUBLIC_ID_CLIENTE || ''
+export const CLIENTSECRET = process.env.NEXT_PUBLIC_SECRET_KEY || ''
