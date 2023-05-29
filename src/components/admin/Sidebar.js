e83fd8ab3f647141ac0ded4b00fffd22f837fcdf
Kevin Segovia <ksegovia555@gmail.com>
2023-05-28 21:40:51 -0600

    change: color hover menu admin

--- src/components/admin/Sidebar.js
+++ src/components/admin/Sidebar.js
@@ -63,25 +63,25 @@ export default function Sidebar ({ selection }) {
             <li>
               <Link href='/admin/dashboard' className='flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 '>
                 <svg aria-hidden='true' className='w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path d='M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z' /><path d='M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z' /></svg>
-                <span className={(selection === 'dashboard') ? 'ml-3 text-rose-400' : 'ml-3'}>Dashboard</span>
+                <span className={(selection === 'dashboard') ? 'ml-3 text-primary' : 'ml-3'}>Dashboard</span>
               </Link>
             </li>
             <li>
               <Link href='/admin/' className='flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 '>
                 <svg aria-hidden='true' className='flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z' clipRule='evenodd' /></svg>
-                <span className={(selection === 'productos') ? 'ml-3 text-rose-400' : 'ml-3'}>Productos</span>
+                <span className={(selection === 'productos') ? 'ml-3 text-primary' : 'ml-3'}>Productos</span>
               </Link>
             </li>
             <li>
               <Link href='/admin/categorias' className='flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 '>
                 <svg aria-hidden='true' className='w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path d='M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z' /><path d='M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z' /></svg>
-                <span className={(selection === 'categorias') ? 'ml-3 text-rose-400' : 'ml-3'}>Categorias</span>
+                <span className={(selection === 'categorias') ? 'ml-3 text-primary' : 'ml-3'}>Categorias</span>
               </Link>
             </li>
             <li>
               <Link href='/admin/pedidos' className='flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 '>
                 <svg aria-hidden='true' className='w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path d='M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z' /><path d='M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z' /></svg>
-                <span className={(selection === 'pedidos') ? 'ml-3 text-rose-400' : 'ml-3'}>Pedidos</span>
+                <span className={(selection === 'pedidos') ? 'ml-3 text-primary' : 'ml-3'}>Pedidos</span>
               </Link>
             </li>
           </ul>
